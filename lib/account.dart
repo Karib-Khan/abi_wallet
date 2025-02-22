2108961c9f74fa2938cf44d6b956dfac45f6bddb
Karib-Khan <abdurrahmanfarukkhan@gmail.com>
2025-02-22 22:41:06 +0600

    Account page done

--- lib/account.dart
+++ lib/account.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'base_scaffold.dart';
+import 'base_scaffold.dart'; // Import the BaseScaffold for consistency
+import 'edit_profile.dart'; // Import the EditProfileScreen
 
 class AccountScreen extends StatefulWidget {
   @override
@@ -7,14 +8,302 @@ class AccountScreen extends StatefulWidget {
 }
 
 class _AccountScreenState extends State<AccountScreen> {
+  // Dummy user data (replace with real data from your backend later)
+  final String userName = "John Doe";
+  final String userEmail = "john.doe@example.com";
+  final String phoneNo = "+1 123-456-7890";
+  final String nid = "1234567890";
+  final String gender = "Male"; // Can be "Male", "Female", or "Other"
+  final String dob = "01/01/1990";
+  final String address = "123 Main St, City, Country";
+  final String bankName = "Example Bank";
+  final String branchName = "Downtown Branch";
+  final String accountHolderName = "John Doe";
+  final String accountNumber = "1234567890";
+  final String profileImagePath = 'lib/assets/imgs/profile.png'; // Placeholder image path
+
   @override
   Widget build(BuildContext context) {
     return BaseScaffold(
-      child: Center(
-        child: Text(
-          'Account Screen',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      initialIndex: 3,
+      child: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0), // Add padding to ensure content isn’t cut off
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Header Section
+            Text(
+              'Account',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10.0), // Add spacing between header and profile section
+
+            // Profile Section
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10.0),
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
+                  // Profile Picture
+                  GestureDetector(
+                    onTap: () {
+                      // Navigate to EditProfileScreen to change profile picture
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => EditProfileScreen(
+                            userName: userName,
+                            userEmail: userEmail,
+                            phoneNo: phoneNo,
+                            nid: nid,
+                            gender: gender,
+                            dob: dob,
+                            address: address,
+                            bankName: bankName,
+                            branchName: branchName,
+                            accountHolderName: accountHolderName,
+                            accountNumber: accountNumber,
+                            profileImagePath: profileImagePath,
+                          ),
+                        ),
+                      );
+                    },
+                    child: CircleAvatar(
+                      radius: 40,
+                      backgroundImage: AssetImage(profileImagePath),
+                      backgroundColor: Colors.grey[300],
+                    ),
+                  ),
+                  SizedBox(width: 16.0),
+                  // User Details
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          userName,
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          userEmail,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // Additional User Information Section
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Personal Information',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                _buildInfoItem('Phone No', phoneNo),
+                _buildInfoItem('NID', nid),
+                _buildInfoItem('Gender', gender),
+                _buildInfoItem('Date of Birth', dob),
+                _buildInfoItem('Address', address),
+              ],
+            ),
+
+            SizedBox(height: 20.0), // Add spacing between sections
+
+            // Banking Section
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Banking',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                _buildInfoItem('Name of Bank', bankName),
+                _buildInfoItem('Name of Branch', branchName),
+                _buildInfoItem('Account Holder Name', accountHolderName),
+                _buildInfoItem('Account Number', accountNumber),
+              ],
+            ),
+
+            SizedBox(height: 20.0), // Add spacing before Settings
+
+            // Account Settings Section
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Settings',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                // Edit Profile Button
+                _buildSettingsItem(
+                  icon: Icons.edit,
+                  title: 'Edit Profile',
+                  onTap: () {
+                    // Navigate to EditProfileScreen
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => EditProfileScreen(
+                          userName: userName,
+                          userEmail: userEmail,
+                          phoneNo: phoneNo,
+                          nid: nid,
+                          gender: gender,
+                          dob: dob,
+                          address: address,
+                          bankName: bankName,
+                          branchName: branchName,
+                          accountHolderName: accountHolderName,
+                          accountNumber: accountNumber,
+                          profileImagePath: profileImagePath,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                // Change Password Button
+                _buildSettingsItem(
+                  icon: Icons.lock,
+                  title: 'Change Password',
+                    iconColor: Colors.deepOrange, // Red icon for logout
+                    textColor: Colors.deepOrange,
+                  onTap: () {
+                    // Add navigation or action for changing password
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Change Password tapped')),
+                    );
+                  },
+                ),
+                // Logout Button (Styled in Red)
+                _buildSettingsItem(
+                  icon: Icons.logout,
+                  title: 'Logout',
+                  iconColor: Colors.red, // Red icon for logout
+                  textColor: Colors.red, // Red text for logout
+                  onTap: () {
+                    // Add logout logic here (e.g., clear user data, navigate to login screen)
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Logged out')),
+                    );
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 100.0), // Add extra space to ensure scrolling reaches the bottom
+          ],
         ),
+      ), // AccountScreen corresponds to index 3 in the bottom navigation bar
+    );
+  }
+
+  Widget _buildInfoItem(String title, String value) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 10),
+      padding: EdgeInsets.all(12.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            value,
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSettingsItem({
+    required IconData icon,
+    required String title,
+    required VoidCallback onTap,
+    Color iconColor = Colors.green, // Default to green, but can be overridden
+    Color textColor = Colors.black, // Default to black, but can be overridden
+  }) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: ListTile(
+        leading: Icon(icon, color: iconColor),
+        title: Text(
+          title,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: textColor,
+          ),
+        ),
+        trailing: Icon(Icons.arrow_forward_ios, color: Colors.grey),
+        onTap: onTap,
       ),
     );
   }
---
+++ lib/edit_profile.dart
@@ -0,0 +1,270 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+class EditProfileScreen extends StatefulWidget {
+  final String userName;
+  final String userEmail;
+  final String phoneNo;
+  final String nid;
+  final String gender;
+  final String dob;
+  final String address;
+  final String bankName;
+  final String branchName;
+  final String accountHolderName;
+  final String accountNumber;
+  final String profileImagePath;
+
+  const EditProfileScreen({
+    required this.userName,
+    required this.userEmail,
+    required this.phoneNo,
+    required this.nid,
+    required this.gender,
+    required this.dob,
+    required this.address,
+    required this.bankName,
+    required this.branchName,
+    required this.accountHolderName,
+    required this.accountNumber,
+    required this.profileImagePath,
+  });
+
+  @override
+  _EditProfileScreenState createState() => _EditProfileScreenState();
+}
+
+class _EditProfileScreenState extends State<EditProfileScreen> {
+  late TextEditingController _nameController;
+  late TextEditingController _emailController;
+  late TextEditingController _phoneController;
+  late TextEditingController _nidController;
+  late TextEditingController _dobController;
+  late TextEditingController _addressController;
+  late TextEditingController _bankNameController;
+  late TextEditingController _branchNameController;
+  late TextEditingController _accountHolderNameController;
+  late TextEditingController _accountNumberController;
+  String _gender = 'Male'; // Default gender
+  File? _profileImage; // Store the selected or captured image
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController(text: widget.userName);
+    _emailController = TextEditingController(text: widget.userEmail);
+    _phoneController = TextEditingController(text: widget.phoneNo);
+    _nidController = TextEditingController(text: widget.nid);
+    _dobController = TextEditingController(text: widget.dob);
+    _addressController = TextEditingController(text: widget.address);
+    _bankNameController = TextEditingController(text: widget.bankName);
+    _branchNameController = TextEditingController(text: widget.branchName);
+    _accountHolderNameController = TextEditingController(text: widget.accountHolderName);
+    _accountNumberController = TextEditingController(text: widget.accountNumber);
+    _gender = widget.gender;
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _phoneController.dispose();
+    _nidController.dispose();
+    _dobController.dispose();
+    _addressController.dispose();
+    _bankNameController.dispose();
+    _branchNameController.dispose();
+    _accountHolderNameController.dispose();
+    _accountNumberController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _pickImage(ImageSource source) async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: source);
+
+    if (pickedFile != null) {
+      setState(() {
+        _profileImage = File(pickedFile.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          'Edit Profile',
+          style: TextStyle(color: Colors.black),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Profile Picture Section
+            Center(
+              child: GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return Container(
+                        padding: EdgeInsets.all(16.0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            ListTile(
+                              leading: Icon(Icons.photo_library),
+                              title: Text('Choose from Gallery'),
+                              onTap: () {
+                                Navigator.pop(context);
+                                _pickImage(ImageSource.gallery);
+                              },
+                            ),
+                            ListTile(
+                              leading: Icon(Icons.camera_alt),
+                              title: Text('Take a Photo'),
+                              onTap: () {
+                                Navigator.pop(context);
+                                _pickImage(ImageSource.camera);
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundImage: _profileImage != null
+                      ? FileImage(_profileImage!) as ImageProvider
+                      : AssetImage(widget.profileImagePath) as ImageProvider,
+                  backgroundColor: Colors.grey[300],
+                  child: _profileImage == null
+                      ? Icon(Icons.camera_alt, color: Colors.white, size: 30)
+                      : null,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+
+            // Personal Information Fields
+            Text(
+              'Personal Information',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            _buildTextField('Name', _nameController),
+            _buildTextField('Email', _emailController),
+            _buildTextField('Phone No', _phoneController),
+            _buildTextField('NID', _nidController),
+            _buildTextField('Date of Birth', _dobController, readOnly: true, onTap: () async {
+              DateTime? pickedDate = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1900),
+                lastDate: DateTime.now(),
+              );
+              if (pickedDate != null) {
+                _dobController.text = "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}";
+              }
+            }),
+            _buildTextField('Address', _addressController),
+            DropdownButtonFormField<String>(
+              value: _gender,
+              decoration: InputDecoration(
+                labelText: 'Gender',
+                border: OutlineInputBorder(),
+              ),
+              items: ['Male', 'Female', 'Other'].map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (newValue) {
+                setState(() {
+                  _gender = newValue!;
+                });
+              },
+            ),
+
+            SizedBox(height: 20),
+            // Banking Information Fields
+            Text(
+              'Banking',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            _buildTextField('Name of Bank', _bankNameController),
+            _buildTextField('Name of Branch', _branchNameController),
+            _buildTextField('Account Holder Name', _accountHolderNameController),
+            _buildTextField('Account Number', _accountNumberController),
+
+            SizedBox(height: 20),
+            // Save Button
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  // Add logic to save changes (e.g., send to backend)
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Profile updated successfully')),
+                  );
+                  Navigator.pop(context); // Return to AccountScreen
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green, // Use backgroundColor instead of primary
+                  minimumSize: Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text(
+                  'Save Changes',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(String label, TextEditingController controller,
+      {bool readOnly = false, VoidCallback? onTap}) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 10),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: label,
+          border: OutlineInputBorder(),
+          enabled: !readOnly,
+        ),
+        readOnly: readOnly,
+        onTap: onTap,
+      ),
+    );
+  }
+}
